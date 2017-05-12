b51b9bf2371f7dd32c36281ab3728a6246d24b23
Marvin <Marvin@172.31.8.41>
2017-05-12 09:51:55 +0200

    Main Klasse Kommentar

--- src/ceeohh2/hellmain/CeeOhh2HellMain.java
+++ src/ceeohh2/hellmain/CeeOhh2HellMain.java
@@ -18,7 +18,7 @@ public class CeeOhh2HellMain
      */
     public static void main ( String[] args )
     {
-        consoleTest();
+        consoleTest(); // test ob pull geht.
     }
     
     private static void consoleTest()
